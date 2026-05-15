7da0f7e782011571774fd201d7f90e5d1f7a829b
Divya Sharma <divyasharma.cs24@bmsce.ac.in>
2026-05-15 09:49:59 +0530

    Create DeadlockDetection.c

---
+++ lab7/DeadlockDetection.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+
+int main() {
+    int n, m, i, j, k;
+
+    // n = processes
+    // m = resources
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter number of resource types: ");
+    scanf("%d", &m);
+
+    int alloc[n][m], request[n][m];
+    int avail[m];
+
+    // Allocation Matrix
+    printf("\nEnter Allocation Matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < m; j++) {
+            scanf("%d", &alloc[i][j]);
+        }
+    }
+
+    // Request Matrix
+    printf("\nEnter Request Matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < m; j++) {
+            scanf("%d", &request[i][j]);
+        }
+    }
+
+    // Available Vector
+    printf("\nEnter Available Resources:\n");
+    for(i = 0; i < m; i++) {
+        scanf("%d", &avail[i]);
+    }
+
+    int finish[n];
+
+    // Initialization
+    for(i = 0; i < n; i++) {
+
+        int zeroAlloc = 1;
+
+        for(j = 0; j < m; j++) {
+            if(alloc[i][j] != 0) {
+                zeroAlloc = 0;
+                break;
+            }
+        }
+
+        if(zeroAlloc)
+            finish[i] = 1;
+        else
+            finish[i] = 0;
+    }
+
+    int found;
+
+    do {
+        found = 0;
+
+        for(i = 0; i < n; i++) {
+
+            if(finish[i] == 0) {
+
+                int possible = 1;
+
+                for(j = 0; j < m; j++) {
+                    if(request[i][j] > avail[j]) {
+                        possible = 0;
+                        break;
+                    }
+                }
+
+                if(possible) {
+
+                    for(k = 0; k < m; k++) {
+                        avail[k] += alloc[i][k];
+                    }
+
+                    finish[i] = 1;
+                    found = 1;
+                }
+            }
+        }
+
+    } while(found);
+
+    // Check deadlock
+    int deadlock = 0;
+
+    printf("\nDeadlocked Processes: ");
+
+    for(i = 0; i < n; i++) {
+        if(finish[i] == 0) {
+            printf("P%d ", i);
+            deadlock = 1;
+        }
+    }
+
+    if(deadlock == 0) {
+        printf("None");
+    }
+
+    printf("\n");
+
+    return 0;
+}
