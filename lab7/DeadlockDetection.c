ca139452a8e4e8a75b8d349a230333b3e13b63a5
Divya Sharma <divyasharma.cs24@bmsce.ac.in>
2026-05-15 11:37:37 +0530

    Create DeadlockDetection.c

--- lab7/DeadlockDetection.c
+++ lab7/DeadlockDetection.c
@@ -1,110 +1,76 @@
 #include <stdio.h>
-
-int main() {
-    int n, m, i, j, k;
-
-    // n = processes
-    // m = resources
+int main(){
+    int n,m,i,j,k;
     printf("Enter number of processes: ");
-    scanf("%d", &n);
-
+    scanf("%d",&n);
     printf("Enter number of resource types: ");
-    scanf("%d", &m);
-
-    int alloc[n][m], request[n][m];
+    scanf("%d",&m);
+    int alloc[n][m],request[n][m];
     int avail[m];
-
-    // Allocation Matrix
     printf("\nEnter Allocation Matrix:\n");
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < m; j++) {
-            scanf("%d", &alloc[i][j]);
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&alloc[i][j]);
         }
     }
-
-    // Request Matrix
     printf("\nEnter Request Matrix:\n");
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < m; j++) {
-            scanf("%d", &request[i][j]);
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&request[i][j]);
         }
     }
-
-    // Available Vector
     printf("\nEnter Available Resources:\n");
-    for(i = 0; i < m; i++) {
-        scanf("%d", &avail[i]);
+    for(i=0;i<m;i++){
+        scanf("%d",&avail[i]);
     }
-
     int finish[n];
-
-    // Initialization
-    for(i = 0; i < n; i++) {
-
-        int zeroAlloc = 1;
-
-        for(j = 0; j < m; j++) {
-            if(alloc[i][j] != 0) {
-                zeroAlloc = 0;
+    for(i=0;i<n;i++){
+        int zeroAlloc=1;
+        for(j=0;j<m;j++){
+            if(alloc[i][j]!= 0){
+                zeroAlloc=0;
                 break;
             }
         }
-
         if(zeroAlloc)
-            finish[i] = 1;
+            finish[i]=1;
         else
-            finish[i] = 0;
+            finish[i]=0;
     }
-
     int found;
-
-    do {
-        found = 0;
-
-        for(i = 0; i < n; i++) {
-
-            if(finish[i] == 0) {
-
-                int possible = 1;
-
-                for(j = 0; j < m; j++) {
-                    if(request[i][j] > avail[j]) {
-                        possible = 0;
+    do{
+        found=0;
+        for(i=0;i<n;i++){
+            if(finish[i]==0){
+                int possible=1;
+                for(j=0;j<m;j++){
+                    if(request[i][j]>avail[j]){
+                        possible=0;
                         break;
                     }
                 }
-
-                if(possible) {
-
-                    for(k = 0; k < m; k++) {
-                        avail[k] += alloc[i][k];
+                if(possible){
+                    for(k=0;k<m;k++){
+                        avail[k]+=alloc[i][k];
                     }
-
-                    finish[i] = 1;
-                    found = 1;
+                    finish[i]=1;
+                    found=1;
                 }
             }
         }
 
-    } while(found);
-
-    // Check deadlock
-    int deadlock = 0;
-
+    }while(found);
+    int deadlock=0;
     printf("\nDeadlocked Processes: ");
-
-    for(i = 0; i < n; i++) {
-        if(finish[i] == 0) {
-            printf("P%d ", i);
-            deadlock = 1;
+    for(i=0;i<n;i++){
+        if(finish[i]==0){
+            printf("P%d ",i);
+            deadlock=1;
         }
     }
-
-    if(deadlock == 0) {
+    if(deadlock==0){
         printf("None");
     }
-
     printf("\n");
-
     return 0;
 }
